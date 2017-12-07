836edf2d6ecb1f4b08a178fc8eab0814e921cd59
milk42 <32737093+milk42@users.noreply.github.com>
2017-12-07 17:37:21 +0100

    Update plug_laetitia.php

--- plug_laetitia.php
+++ plug_laetitia.php
@@ -4,12 +4,14 @@ Plugin Name: Zero plugin
 Plugin URI: http://zero-plugin.com
 Description: Un plugin d'introduction pour le développement sous WordPress
 Version: 0.1
-Author: Moi
-Author URI: http://votre-site.com
+Author: Laetitia
+Author URI: http://7715460bf2c140cd84058813a164cc41.yatu.ws
 License: LP2
 */
 
-function maFonctionPlugin() {
-	return '<p>Voila ce que retourne mon extension.</p>';
+function shortCodePlugin() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire simplon.co - une entreprise de
+l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+des formations GRATUITES pour devenir développeur web.</p>';
 }
-add_shortcode('simplon', 'maFonctionPlugin');
+add_shortcode('simplon2', 'shortCodePlugin');
